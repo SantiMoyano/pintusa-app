1044f4b6a3745b9c48eae4322a287c1fd5ffa3cc
Santiago Moyano <santisebmoyanosuarez@gmail.com>
2023-10-19 18:27:15 -0300

    Implement drawing tools functionality

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@ import Canvas from "./Canvas.js";
 import Pencil from "./Pencil.js";
 
 const canvasElement = document.querySelector("#canvas");
+const canvasClear = document.querySelector("#clear-canvas");
+const eraserTool = document.querySelector("#eraser");
+const pencilTool = document.querySelector("#pencil");
+const colorTool = document.querySelector("#color");
+const lineWidthTool = document.querySelector("#lineWidth");
 
 // Create a new pencil and canvas
 const pencil = new Pencil();
@@ -19,4 +24,35 @@ const mouseMoving = (e) => {
 
 canvasElement.addEventListener("mouseup", function () {
   canvasElement.removeEventListener("mousemove", mouseMoving);
+});
+
+// Clear canvas
+canvasClear.addEventListener("mousedown", function () {
+  canvas.clearCanvas();
+});
+
+// Use eraser
+eraserTool.addEventListener("mousedown", function () {
+  pencil.setGrosor(50);
+  pencil.setColor("#fff");
+  canvas.initPencil();
+});
+
+// Use pencil
+pencilTool.addEventListener("mousedown", function () {
+  pencil.setGrosor(10);
+  pencil.setColor("#000");
+  canvas.initPencil();
+});
+
+// Change color
+colorTool.addEventListener("input", function (e) {
+  pencil.setColor(e.target.value);
+  canvas.initPencil();
+});
+
+// Set line width
+lineWidthTool.addEventListener("change", function (e) {
+  pencil.setGrosor(e.target.value * 10);
+  canvas.initPencil();
 });
